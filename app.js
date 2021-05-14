c6da3bb3269e1452d1b708a8a16cd5afe28d2d1e
divyansh <dahiya.divyansh1@gmail.com>
2021-05-14 22:44:50 +0530

    2(Delete Note Functional)

--- app.js
+++ app.js
@@ -42,3 +42,16 @@ function showNotes() {
     notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
   }
 }
+
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
+}
